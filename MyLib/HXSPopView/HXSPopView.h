b8ec62ab966ff11233140a0b27d3cd764017f75d
黎明 <suzw@59store.com>
2016-07-13 15:58:19 +0800

    aaaa

---
+++ MyLib/HXSPopView/HXSPopView.h
@@ -0,0 +1,15 @@
+//
+//  HXSPopView.h
+//  aaaaaa
+//
+//  Created by  黎明 on 16/7/12.
+//  Copyright © 2016年 suzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXSPopView : UIView
+
+- (instancetype)initWithUrl:(NSString *)imageUrl;
+- (void)show;
+@end
---
+++ MyLib/HXSPopView/HXSPopView.m
@@ -0,0 +1,90 @@
+//
+//  HXSPopView.m
+//  aaaaaa
+//
+//  Created by  黎明 on 16/7/12.
+//  Copyright © 2016年 suzw. All rights reserved.
+//
+
+#import "HXSPopView.h"
+
+
+@interface HXSPopView()
+
+@property (nonatomic, weak) IBOutlet UIView *popBgView;
+@property (nonatomic, weak) IBOutlet UIImageView *popContentImageView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *popContentHeightConstraint;
+
+@property (nonatomic, strong) NSString *imageUrl;
+
+@end
+
+@implementation HXSPopView
+
+- (void)awakeFromNib
+{
+    [self.popContentImageView.layer setCornerRadius:5.0f];
+    [self.popContentImageView.layer setMasksToBounds:YES];
+
+    [self.popContentImageView setBackgroundColor:[UIColor whiteColor]];
+    [self.popContentImageView setContentMode:UIViewContentModeScaleAspectFill];
+}
+
+- (instancetype)initWithUrl:(NSString *)imageUrl
+{
+    self = [[[NSBundle mainBundle] loadNibNamed:NSStringFromClass([self class]) owner:nil options:nil] firstObject];
+    
+    if (self)
+    {
+        self.frame = [[UIScreen mainScreen] bounds];
+        self.imageUrl = imageUrl;
+        [self setupSubViews];
+    }
+    return self;
+}
+
+- (void)setupSubViews
+{
+    NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:self.imageUrl]];
+    self.popContentImageView.image = [UIImage imageWithData:data];
+}
+
+
+- (IBAction)closeButtonClickAction:(id)sender
+{
+    [self close];
+}
+
+- (void)show
+{
+    UIWindow *window = [[UIApplication sharedApplication] keyWindow];
+    [window addSubview:self];
+
+    
+    [self layoutIfNeeded];
+    [UIView animateWithDuration:.5 animations:^{
+    
+        self.popContentHeightConstraint.constant = CGRectGetWidth(self.bounds);
+        [self layoutIfNeeded];
+
+    }];
+}
+
+- (void)close
+{
+    self.popContentHeightConstraint.constant = 0;
+
+    [UIView animateWithDuration:.5 animations:^{
+        
+        [self layoutIfNeeded];
+    } completion:^(BOOL finished) {
+     
+        [UIView animateWithDuration:.3 animations:^{
+            [self setAlpha:0];
+        } completion:^(BOOL finished) {
+            [self removeFromSuperview];
+        }];
+    }];
+}
+
+@end
